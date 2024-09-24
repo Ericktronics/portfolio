fe48c7c6b7d0d3317547bfcd855f1b36f0d9548c
Ericktronics <johnhenrickperalta@gmail.com>
2024-09-24 20:52:30 +0800

    init home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  yoe: number = 0;
+  ngOnInit(): void {
+    const date = new Date();
+    const startDate = {
+      year: 2020,
+      month: 11,
+    };
 
+    this.yoe = +date.getFullYear() - startDate.year;
+  }
 }
