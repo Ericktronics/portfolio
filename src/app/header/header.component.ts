43ad4f3b66f820101f7cc7359fa14c71162a8b99
Ericktronics <johnhenrickperalta@gmail.com>
2025-06-22 17:55:27 +0800

    feature: responsive web

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,37 @@ import { Component, HostListener } from '@angular/core';
 })
 export class HeaderComponent {
   isScrolled = false;
+  menuOpen = false;
+
+  @ViewChild('menu', { static: false }) menuRef!: ElementRef;
+
+  constructor(private eRef: ElementRef) {}
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 20;
+    this.closeMenu();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    if (
+      this.menuOpen &&
+      this.menuRef &&
+      !this.menuRef.nativeElement.contains(event.target) &&
+      !this.eRef.nativeElement
+        .querySelector('.header__hamburger')
+        .contains(event.target)
+    ) {
+      this.closeMenu();
+    }
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
   }
 }
